6e75a747578ee5d814d9c86cf4470036577393da
Pasya123 <pasya123@gmail.com>
2020-09-04 01:43:29 +0300

    debug gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('sass', function(){
     //  .pipe(rename({suffix: '.min'}))
         .pipe(autoprefixer()) // после компиляции sass кода - вызываем autoprefixer для расставления префиксов
         .pipe(gulp.dest('app/css')) // указываем куда перемещать компилированные в .css файлы
-        .pipe(browserSync.reload({stream: true})) // обновляет локальный сервер при выполнении таска
+        .pipe(browserSync.reload({stream: true})); // обновляет локальный сервер при выполнении таска
 });
 
 gulp.task('autoprefixer', function(){
@@ -22,17 +22,17 @@ gulp.task('autoprefixer', function(){
 			overrideBrowserslist: ['last 5 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('html', function(){ // обновляет локальный сервер при выполнении таска
     return gulp.src('app/*.html')
-        .pipe(browserSync.reload({stream: true})) 
+        .pipe(browserSync.reload({stream: true})); 
 });
 
 gulp.task('script', function(){ // обновляет локальный сервер при выполнении таска
     return gulp.src('app/js/*.js')
-        .pipe(browserSync.reload({stream: true})) 
+        .pipe(browserSync.reload({stream: true})); 
 });
 
 gulp.task('js', function(){
@@ -43,7 +43,7 @@ gulp.task('js', function(){
         .pipe(concat('libs.min.js')) // обьединяем все файлы через concat в файл с указанным именем
         .pipe(uglify()) // сжимаем обьединённый файл через uglify
         .pipe(gulp.dest('app/js')) // выбрасываем готовый файл в указанную папку
-        .pipe(browserSync.reload({stream: true})) // обновляет локальный сервер при выполнении таска (таск выше выполняется командой 'gulp js)
+        .pipe(browserSync.reload({stream: true})); // обновляет локальный сервер при выполнении таска (таск выше выполняется командой 'gulp js)
 });
 
 gulp.task('browser-sync', function(){
@@ -55,9 +55,9 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')) // функция: мониторить указанную папку галпу и при изменении указанных файлов тут же запускается таск 'gulp sass'
-    gulp.watch('app/*.html', gulp.parallel('html')) // task по отслеживанию изменений в html и обновлении локального сервера при изменениях в них через запуск таска 'html'
-    gulp.watch('app/js/*.js', gulp.parallel('script')) // task по отслеживанию изменений в js и обновлении локального сервера при изменениях в них через запуск таска 'script'
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); // функция: мониторить указанную папку галпу и при изменении указанных файлов тут же запускается таск 'gulp sass'
+    gulp.watch('app/*.html', gulp.parallel('html')); // task по отслеживанию изменений в html и обновлении локального сервера при изменениях в них через запуск таска 'html'
+    gulp.watch('app/js/*.js', gulp.parallel('script')); // task по отслеживанию изменений в js и обновлении локального сервера при изменениях в них через запуск таска 'script'
 });
 
-gulp.task('default', gulp.parallel('sass', 'autoprefixer', 'js', 'browser-sync', 'watch')) // стандартный таск (вызывающийся командой 'gulp'), выполняющий таски: sass, js, browser-sync, watch, autoprefixer
+gulp.task('default', gulp.parallel('sass', 'autoprefixer', 'js', 'browser-sync', 'watch')); // стандартный таск (вызывающийся командой 'gulp'), выполняющий таски: sass, js, browser-sync, watch, autoprefixer
