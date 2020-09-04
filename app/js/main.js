47fd68fb67d322d74d72e5e4b340f8b2dfe165c5
Pasya123 <pasya123@gmail.com>
2020-09-05 02:20:31 +0300

    function learn

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,72 @@
 "use strict";
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+let numberOfFilms;
 
-// for(let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//           b = prompt('На сколько оцените его?', '');
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
-// }
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
-// if (personalMovieDB.count < 10) {
-//     console.log('Просмотренно слишком мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-// } else if (personalMovieDB.count > 30) {
-//     console.log('Вы киноман');
-// } else {
-//     console.log('error');
-// }
+start();
 
-// console.log(personalMovieDB);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+//rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотренно слишком мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('error');
+    }
+}
+
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
 
 
 
@@ -40,13 +74,9 @@
 
 // let n = +prompt('Загадайте число от 1 до 50', '');
 
-let input = document.querySelector('.num-form');
-console.log(input.value);
-
-
 // for (let i = 0; i < 5; i++) {
 //     let attempts = 5;
-//     let a = +prompt('Угадайте число. Осталось попыток: ' + (attempts - i), '');
+//     let a = +prompt(`Угадайте число. Осталось попыток: ${attempts - i}`);
 //     if (a < 0 && a > 50) {
 //         console.log('Число нужно вводить от 0 до 50');
 //     }
